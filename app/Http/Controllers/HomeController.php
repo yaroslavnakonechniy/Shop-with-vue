90563f555dcbbc6bb613fe7bfc02d89e78fcac7b
yaroslavn <yaroslav.n@open-tag.com>
2024-07-05 17:03:55 +0300

    Registration for admin: fixed routes for layout and MainControoller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('layout.main');
+        return redirect()->route('layout.main');
     }
 }
