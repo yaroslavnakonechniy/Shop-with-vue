cad6f19b840ee5d52d90cb59eb5c0994879c56e2
yaroslavn <yaroslav.n@open-tag.com>
2024-07-06 15:20:58 +0300

    Pagination: created pagination for categories on main page and admin panel. a

--- app/Http/Controllers/Main/MainController.php
+++ app/Http/Controllers/Main/MainController.php
@@ -19,7 +19,7 @@ class MainController extends Controller
 
     public function categoriesShow(){
 
-        $categories = Category::all();
+        $categories = Category::paginate(5);
 
         return view('main.categories.index', compact('categories'));
     }
