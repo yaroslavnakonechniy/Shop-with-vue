c17f93213ee7baf289dc902a4a7abc49174c5025
yaroslavn <yaroslav.n@open-tag.com>
2024-07-26 23:42:14 +0300

    Basket is not empty: added middleware for basket, redirected users on main page when basket is empty.

---
+++ app/Http/Middleware/BasketIsNotEmpty.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\Order\Order;
+
+class BasketIsNotEmpty
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $orderId = session('orderId');
+        if (!is_null($orderId)) {
+            $order = Order::find($orderId);
+            if($order->products->count() == 0){
+                session()->flash('warning', 'Basket is empty!!');
+                return redirect()->route('layout.main');
+            }
+        }
+
+        return $next($request);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -54,11 +54,15 @@ Route::group(['namespace' => 'Main'], function () {
 
 Route::group(['namespace' => 'Cart', 'prefix' => 'cart'], function () {
 
-    Route::get('/index', 'CartController@index')->name('cart.index');
+    Route::group(['middleware' => 'basket-is-not-empty'], function () {
+        Route::get('/index', 'CartController@index')->name('cart.index');
+        Route::get('/confirm/form', 'CartController@confirmForm')->name('confirm.form');
+        Route::post('/confirm/order', 'CartController@confirmOrder')->name('cart.confirm');
+    });
+    
     Route::post('/add/{id}', 'CartController@addProduct')->name('cart.add');
     Route::post('/remove/{id}', 'CartController@removeProduct')->name('cart.remove');
-    Route::get('/confirm/form', 'CartController@confirmForm')->name('confirm.form');
-    Route::post('/confirm/order', 'CartController@confirmOrder')->name('cart.confirm');
+    
 });
 
 
