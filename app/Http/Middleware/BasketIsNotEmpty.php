627d931790b648f87af0584196bda4eedfe54008
yaroslavn <yaroslav.n@open-tag.com>
2024-07-30 23:19:26 +0300

    User orders: bug with basket.

--- app/Http/Middleware/BasketIsNotEmpty.php
+++ app/Http/Middleware/BasketIsNotEmpty.php
@@ -17,6 +17,7 @@ class BasketIsNotEmpty
      */
     public function handle(Request $request, Closure $next)
     {
+        /*
         $orderId = session('orderId');
         if (!is_null($orderId)) {
             $order = Order::find($orderId);
@@ -26,5 +27,18 @@ class BasketIsNotEmpty
         }
         session()->flash('warning', 'Basket is empty!!');
         return redirect()->route('layout.main');
+        
+        */
+
+        $orderId = session('orderId');
+        if (!is_null($orderId)) {
+            $order = Order::find($orderId);
+            if($order->products->count() == 0){
+                session()->flash('warning', 'Basket is empty!!');
+                return redirect()->route('layout.main');
+            }
+        }
+
+        return $next($request);
     }
 }
